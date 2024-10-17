58e50cdbd91b3fb1d9f2f0555f0db6685ddd48c9
JuanMurcia1 <Juan_esteban.murcia@uao.edu.co>
2024-10-17 14:53:48 -0500

    Third commit
    Arreglando bugs de halos de luz y agregaon luz a los objetos que interactuan con el desfibrilador

--- Assets/Scripts-CONTROLADOR/Desfibrilador.cs
+++ Assets/Scripts-CONTROLADOR/Desfibrilador.cs
@@ -5,23 +5,99 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class Desfibrilador : MonoBehaviour
 {
-    public Light areaLight;  // Referencia a tu Area Light
+    public Light areaLight;  
+    public Light areaLightmani;
+    public Light lightMonitoriza;
+    public Light lightSPO2;
+    
+    public Light lightPresion;
+    public Light lightDEA;
+    
 
     private void Start()
     {
+        
         // Asegúrate de que la luz esté apagada inicialmente si es necesario
         areaLight.enabled = false;
+        areaLightmani.enabled = false;
+        lightMonitoriza.enabled = false;
+        lightDEA.enabled = false;
+        lightPresion.enabled = false;
+        lightSPO2.enabled = false;
     }
 
    
 
      public void OnHoverEntered(HoverEnterEventArgs args)
     {
-       areaLight.enabled = true;
+         if (args.interactable.gameObject.tag == "Maniqui")
+        {
+            Debug.Log("maniqui");
+            
+            areaLightmani.enabled = true;   
+        }else if ( args.interactable.gameObject.tag == "Desfibrilador")
+        {
+            Debug.Log("desfibrilador");
+            areaLight.enabled = true;
+
+        }else if ( args.interactable.gameObject.tag == "Monitoriza")
+        {
+            
+            lightMonitoriza.enabled = true;
+
+        }else if ( args.interactable.gameObject.tag == "Presion")
+        {
+            
+            lightPresion.enabled = true;
+
+        }else if ( args.interactable.gameObject.tag == "SPOA")
+        {
+            Debug.Log ("SP");
+            lightSPO2.enabled = true;
+
+        }else if ( args.interactable.gameObject.tag == "DEA")
+        {
+            
+            lightDEA.enabled = true;
+
+        }
+
+       
+       
     }
 
     public void OnHoverExited(HoverExitEventArgs args)
     {
-        areaLight.enabled = false;
+        if (args.interactable.gameObject.tag == "Maniqui")
+        {
+            
+            areaLightmani.enabled = false;   
+        }else if ( args.interactable.gameObject.tag == "Desfibrilador")
+        {
+            areaLight.enabled = false;
+        }else if ( args.interactable.gameObject.tag == "Monitoriza")
+        {
+            
+            lightMonitoriza.enabled = false;
+
+        }else if ( args.interactable.gameObject.tag == "Presion")
+        {
+            
+            lightPresion.enabled = false;
+
+        }else if ( args.interactable.gameObject.tag == "SPOA")
+        {
+            
+            lightSPO2.enabled = false;
+
+        }else if ( args.interactable.gameObject.tag == "DEA")
+        {
+            
+            lightDEA.enabled = false;
+
+        }
     }
+   
+
+
 }
