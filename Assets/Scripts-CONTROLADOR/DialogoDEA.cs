dd055e53ee9e95335cf3d4dc630878cf8f065c5d
JuanHidalgoB <juan.hidalgo@uao.edu.co>
2024-11-07 01:19:25 -0500

    Actualiza DEA.unity y DialogoDEA.cs desde juank a main

--- Assets/Scripts-CONTROLADOR/DialogoDEA.cs
+++ Assets/Scripts-CONTROLADOR/DialogoDEA.cs
@@ -6,6 +6,12 @@ using TMPro;
 
 public class DialogoDEA : MonoBehaviour
 {
+    private AudioSource audioSource;
+    public GameObject[] flecha;
+    public GameObject DEA2;
+    public GameObject correctPlace;
+    public GameObject perillaOff;
+    public GameObject perillaDEA;
     public Desfibrilador dea;
     public TextMeshProUGUI instruccion;
     private int indicador;
@@ -13,36 +19,69 @@ public class DialogoDEA : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        instruccion.fontSize = 70;
+        instruccion.alignment = TextAlignmentOptions.Center;
         PasoNext = true;
         indicador = 1;
+        audioSource = perillaDEA.GetComponent<AudioSource>();
+        PasosSiguientes();
     }
 
     // Update is called once per frame
     void Update()
     {
-        instruccion.fontSize = 70;
-        instruccion.alignment = TextAlignmentOptions.Center;
+        
         actualizacionIndicador();
 
+        
         PasosSiguientes();
+        
+        
+       
     }
      public void PasosSiguientes()
     {
         if (indicador == 1){
-            instruccion.text = "Bienvenido al módulo de Desfibrilación Externo Automático, aquí aprenderás a configurar la interfaz y utilizar algunos elementos de bioinstrumentación \n\n\n Presiona H para comenzar con la simulación. ";
+            instruccion.text = "Bienvenido al módulo de Desfibrilación Externo Automático, aquí aprenderás a configurar la interfaz y utilizar algunos elementos de bioinstrumentación. \n\n\n Presiona H para comenzar con la simulación. ";
         }else if (indicador == 2)
         {
-            instruccion.text = "A tu derecha podrás ver el elemento con el cual vas a interactuar." + " Cuando agarres el objeto, podrás ver la información del mismo en esta pantalla." + 
+            instruccion.text = "A tu derecha podrás ver el elemento con el cual vas a interactuar." + " Cuando agarres el objeto, podrás ver la información de el mismo en esta pantalla." + 
             " \n\n Cuando hayas terminado presiona H ";
         }else if (indicador ==3)
         {
-            instruccion.text = "perfecto!, ahora vamos a empezar a colocar el DEA en el desfibrilador y maniquí \n\n\n Presiona H para ver el orden correcto en el que tienes que colocar";
+            instruccion.text = "¡Perfecto! Ahora vamos a empezar a colocar el DEA en el desfibrilador y maniquí \n\n\n Presiona H para ver el orden correcto en el que tienes que colocar";
+
+            correctPlace.SetActive(true);
 
         }else if (indicador ==4)
         {
-            instruccion.text = "el orden es: \n\n  1.Electrodos \n\n 2.Interfaz de pulsioximetría \n\n 3.La interfaz de tensión arterial";
+            instruccion.text = "Primero conecta el cable de los parches al desfibrilador, busca en la mesa a tu derecha el elemento y llevalo a donde indica la flecha roja";
+            flecha[0].SetActive(true);
             dea.lightDEA.enabled = true;
+            PasoNext=false;
 
+            if (DEA2.activeSelf){
+                indicador++;
+                Debug.Log(indicador);
+                flecha[0].SetActive(false);
+            }
+
+        }else if (indicador ==5)
+        {
+            instruccion.text = "Ahora gira la perilla que te indica la flecha roja para colocarlo en modo Desfinbrilador Externo Automático";
+            flecha[1].SetActive(true);
+            if (perillaDEA.activeSelf){
+                indicador++;
+                audioSource.Play();
+                Debug.Log(indicador);
+                flecha[1].SetActive(false);
+            }
+        }else if(indicador ==6)
+        {
+            instruccion.text = "Muy bien, ahora vamos a esperar a que el monitor analice el estado del paciente para saber si es necesaria una descarga electrica.";
+        }else if(indicador == 100){
+
+            instruccion.text = "Un DEA es un tipo de desfibrilador computarizado que analiza automáticamente el ritmo cardiaco en personas que están sufriendo un paro. Cuando sea necesario, envía una descarga eléctrica al corazón para normalizar su ritmo.  La conversión de una arritmia ventricular a su ritmo normal mediante una descarga eléctrica se denomina desfibrilación.";
         }
     }
 
@@ -52,7 +91,47 @@ public class DialogoDEA : MonoBehaviour
         {
             indicador ++;
             PasosSiguientes();
+            Debug.Log(indicador);
         }
         
     }
+
+    public void OnHoverEntered(HoverEnterEventArgs args)
+    {
+
+         if(args.interactable.gameObject.tag == "PerillaOff" && indicador == 5){
+
+            Debug.Log("OnHover detectado");
+            perillaDEA.SetActive(true);
+            perillaOff.SetActive(false);
+         }
+        
+    }
+
+    public void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        if (args.interactable.gameObject.tag == "DEA" && indicador <= 2)
+        {
+            indicador=100;
+            instruccion.text = "Un DEA es un tipo de desfibrilador computarizado que analiza automáticamente el ritmo cardiaco en personas que están sufriendo un paro. Cuando sea necesario, envía una descarga eléctrica al corazón para normalizar su ritmo.  La conversión de una arritmia ventricular a su ritmo normal mediante una descarga eléctrica se denomina desfibrilación.";
+        }
+        
+    }
+
+    public void OnSelectExited(SelectExitEventArgs args)
+    {
+        if (args.interactable.gameObject.tag == "DEA")
+        {
+            
+            if (indicador == 100)
+            {
+            indicador = 2;
+            }
+        }
+        
+        
+        
+        PasosSiguientes();
+    }
+
 }
